91e69be8098572f7109bf9780bb23e81b38f3117
Aman Arya <125aryaaman@gmail.com>
2023-04-26 10:45:49 +0530

    initial

---
+++ App.js
@@ -0,0 +1,13 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import MyStack from './navigation';
+
+function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,14 @@
+# Quiz App
+
+- Made using React-Native
+
+# Contributors
+
+- Aman Arya 0801IT201010
+- Amaan Ur Rehmaan 0801IT201009
+- Ayush Upadhyay 0801IT201026
+
+# Mentored by: 
+
+- DR. LALIT PUROHIT
+- MR. UPENDRA SINGH
